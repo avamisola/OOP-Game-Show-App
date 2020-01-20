8bdd6a2505bab642ae302e569b0ad55f72b6e9e5
avamisola <58097632+avamisola@users.noreply.github.com>
2020-01-20 17:25:06 -0500

    project steps 1-5
    -created Game and Phrases classes
    -output random phrases in lowercase

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Walking on Sunshine'),
+            new Phrase('Genie in a Bottle'),
+            new Phrase('What a Wonderful World'),
+            new Phrase('Singing in the Rain'),
+            new Phrase('Chicken Noodle Soup')
+        ];
+        this.activePhrase = null;
+    }
+    startGame() {
+    
+    }
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    }
+    handleInteraction() {
+    
+    }
+    checkForWin() {
+    
+    }
+    removeLife() {
+    
+    }
+    gameOver() {
+    
+    } 
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game = new Game();
+
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+};
+
+
+
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+
